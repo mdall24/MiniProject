d97214b7c74526716411d6841fe275b64fac4460
Mikkel Kibsgaard Dall <mdall24@student.aau.dk>
2026-04-21 17:51:17 +0200

    started 8ball

--- app/src/main/java/com/example/miniproject/MainActivity.java
+++ app/src/main/java/com/example/miniproject/MainActivity.java
@@ -1,24 +1,28 @@
 package com.example.miniproject;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnEightBall = findViewById(R.id.btnEightBall);
+        Button btnFortune = findViewById(R.id.btnFortune);
+
+        btnEightBall.setOnClickListener(v -> {
+            Intent intent = new Intent(this, eight_ball.class);
+            startActivity(intent);
+        });
+
+        btnFortune.setOnClickListener(v -> {
+            Intent intent = new Intent(this, fortune_teller.class);
+            startActivity(intent);
         });
     }
 }
--- app/src/main/java/com/example/miniproject/eight_ball.java
+++ app/src/main/java/com/example/miniproject/eight_ball.java
@@ -1,11 +1,23 @@
 package com.example.miniproject;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-public class eight_ball {
+import java.util.Random;
+public class eight_ball extends AppCompatActivity {
+
+
+    private final String[] responses = {
+            "Yes",
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.eight_ball);
+    }
+
+    TextView tvAnswer = findViewById(R.id.tvAnswer);
+    Button btnAsk = findViewById(R.id.btnAsk);
 }
