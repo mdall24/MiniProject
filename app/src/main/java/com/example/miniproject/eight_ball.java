f6c3ee4558a10e07a86eb3ae7acd52eabf3748f7
Mikkel Kibsgaard Dall <180258052+mdall24@users.noreply.github.com>
2026-04-27 14:49:34 +0200

    Fix 8ball

--- app/src/main/java/com/example/miniproject/eight_ball.java
+++ app/src/main/java/com/example/miniproject/eight_ball.java
@@ -2,50 +2,70 @@ package com.example.miniproject;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
 import java.util.Random;
-private SensorManager mSensorManager;
-private Sensor mSensor;
 
 
-
-public class eight_ball extends AppCompatActivity {
-
+public class eight_ball extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor gravitySensor;
 
     private final String[] responses = {
-            "Maybe", "Possibly", "Perchance", "Mayhaps", "Perhaps", "No", "Definitely Not", "Negatory", "Never", "Nope","Yes", "Guaranteed", "100 %", "Sure", "Certainly"
+            "Maybe", "Possibly", "Perchance", "Mayhaps", "Perhaps", "No", "Definitely Not", "Negatory", "Never", "Nope", "Yes", "Guaranteed", "100 %", "Sure", "Certainly"
     };
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.eight_ball);
+        setContentView(R.layout.activity_eight_ball);
+
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        gravitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+
+        if (gravitySensor == null) {
+            Toast.makeText(this, "Gravity sensor not available on this device", Toast.LENGTH_LONG).show();
+            finish(); 
+        }
 
         TextView tvAnswer = findViewById(R.id.tvAnswer);
         Button btnAsk = findViewById(R.id.btnAsk);
 
-        btnAsk.setOnClickListener(v-> {
+        btnAsk.setOnClickListener(v -> {
             int index = new Random().nextInt(responses.length);
             tvAnswer.setText(responses[index]);
         });
-        mSensorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        if(mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)!=null){
-            List<Sensor> gravSensor = mSensorManager.getSensorList(Sensor.TYPE_GRAVITY);
-            for(int i = 0; i < gravSensor.size(); i++){
-                if((gravSensor.get(i).getVendor().contains("Google")) && (gravSensor.get(i).getVersion() == 3)){
-                    mSensor = gravSensor.get(i);
-                    break;
-                }
-            }
-        }
-        else if(mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)!=null){
-                mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            }
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, gravitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_GRAVITY){
+            float gravityX = event.values[0];
+            float gravityY = event.values[1];
+            float gravityZ = event.values[2];
         }
     }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+}
